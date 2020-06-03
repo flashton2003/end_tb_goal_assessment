5c48e3d6ad9198b23ca8ee1d8c7ae2d78e3d55b2
Jaeyoon Cha <jaeyooncha@Jaeyoons-MacBook-Air.local>
2020-06-02 22:23:50 -0500

    done for now

--- 000_source-functions.R
+++ 000_source-functions.R
@@ -3,7 +3,6 @@ library(reshape)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
-# to have only one legend for the entire plot grid
 library(cowplot)
 
 ### dataframes from 2000 to 2018 by country
@@ -46,7 +45,6 @@ prev <- gather_(master.prev, keycol, valuecol, gathercols)
 # get rid of the "X" (change X2001 to 2001)
 prev$year <- as.numeric(substr(prev$year, 2, 5))
 
-
 ### all_countries is a list of the 40 countries of interest
 all_countries <- c("Angola","Bangladesh","Brazil","Botswana","Cambodia","Cameroon",
                    "Central African Republic","Chad","China","Congo",
@@ -64,11 +62,10 @@ hiv_13 <- c("Angola", "Cameroon", "Congo", "Eswatini", "Kenya", "Lesotho", "Mala
            "Namibia", "Sierra Leone", "South Africa", "Uganda", 
            "United Republic of Tanzania", "Zimbabwe")
 
-# years used to project TB in 002_TB_projections
+### years used to project TB in 002_TB_projections
 # each of the 40 countries has a corresponding year
 years <- c(2011, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2010, 2000, 2012, 2015, 2000, 2000, 2005, 2000, 2000, 2010, 2000, 2007, 2013, 2015, 2012, 2009, 2011, 2000, 2012, 2000, 2016, 2011, 2009, 2009, 2016, 2000, 2012, 2006, 2000, 2000, 2000)
 names(years) <- c("Angola", "Bangladesh", "Botswana", "Brazil", "Cambodia", "Cameroon", "Central African Republic", "Chad", "China", "Congo", "Democratic People's Republic of Korea", "Democratic Republic of the Congo", "Eswatini", "Ethiopia", "Ghana", "Guinea-Bissau", "India", "Indonesia", "Kenya", "Laos", "Lesotho", "Liberia", "Malawi", "Mozambique", "Myanmar", "Namibia", "Nigeria", "Pakistan", "Papua New Guinea", "Philippines", "Republic of Korea", "Russian Federation", "Sierra Leone", "South Africa", "Thailand", "Uganda", "United Republic of Tanzania", "Vietnam", "Zambia", "Zimbabwe")
-
 
 ### countries grouped by shape
 # used in 001_TB_incidence_trends
@@ -80,15 +77,17 @@ increasing <- c("Angola", "Guinea-Bissau", "Liberia", "Mozambique")
 flat <- c("Bangladesh", "Nigeria", "Democratic People's Republic of Korea", "Papua New Guinea")
 # 11 peak in the 2000s (Figure CWPITN from 001)
 peak_in_00s <- c('Angola', 'Cameroon', 'Congo', 'Eswatini', 'Kenya', 'Lesotho', 'Malawi', 'Namibia', 'South Africa', 'United Republic of Tanzania', 'Zimbabwe')
+# 8 "other" countries (Figures TIFOC and TIFOCNCI from 001)
+other <- c("Philippines", "Republic of Korea", "Democratic Republic of the Congo", 
+           "Pakistan", "Russian Federation", "Uganda", "Myanmar", "Brazil")
 
-# throw warning if input country is not in the 40 countries of interest 
+### throw warning if input country is not in the 40 countries of interest 
 throwWarning <- function(country_name)
 {
   if (country_name %in% all_countries == FALSE) warning('The input country is not among the 40 countries of interest')
-  
 }
 
-# shorten country name to fit in graph; use vernacular names
+### shorten country name to fit in graph; use vernacular names
 # DRC, Central African Republic, Tanzania, Russia, South Korea, North Korea
 shorten_country_name <- function(country_name){
   if (country_name == "Democratic Republic of the Congo"){
@@ -111,5 +110,3 @@ shorten_country_name <- function(country_name){
   }
   return(country_name)
 }
-
-
