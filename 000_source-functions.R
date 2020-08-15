7f17d1be3578a74a29ffa0e5f13f65c907f83d59
Jaeyoon Cha <jaeyoonc@princeton.edu>
2020-08-15 02:09:34 -0500

    list all packages in source file; clean up

--- 000_source-functions.R
+++ 000_source-functions.R
@@ -1,9 +1,15 @@
-library(gridExtra)
-library(reshape)
 library(dplyr)
-library(tidyr)
+library(grid)
+library(gridExtra)
 library(ggplot2)
 library(cowplot)
+library(ggfortify)
+library(reshape)
+library(tidyverse)
+library(knitr)
+library(tibble)
+library(ggpmisc)
+library(tidyr)
 
 ### dataframes from 2000 to 2018 by country
 
@@ -45,7 +51,7 @@ prev <- gather_(master.prev, keycol, valuecol, gathercols)
 # get rid of the "X" (change X2001 to 2001)
 prev$year <- as.numeric(substr(prev$year, 2, 5))
 
-### all_countries is a list of the 40 countries of interest
+### all_countries: list of the 40 high-burden countries of interest
 all_countries <- c("Angola","Bangladesh","Brazil","Botswana","Cambodia","Cameroon",
                    "Central African Republic","Chad","China","Congo",
                    "Democratic People's Republic of Korea",
@@ -57,34 +63,32 @@ all_countries <- c("Angola","Bangladesh","Brazil","Botswana","Cambodia","Cameroo
                    "Russian Federation","Sierra Leone","South Africa","Thailand",
                    "Uganda","United Republic of Tanzania","Vietnam", "Zambia","Zimbabwe")
 
-### hiv_13 is a list of 13 countries analyzed for HIV (Figures HPAUH and )
-hiv_13 <- c("Angola", "Botswana", "Cameroon", "Congo", "Eswatini", "Kenya", "Lesotho", "Malawi", "Namibia", "Sierra Leone", "South Africa", "Uganda", "United Republic of Tanzania", "Zambia", "Zimbabwe")
+### hiv_15: list of 15 countries analyzed for HIV
+hiv_15 <- c("Angola", "Botswana", "Cameroon", "Congo", "Eswatini", "Kenya", "Lesotho", "Malawi", "Namibia", "Sierra Leone", "South Africa", "Uganda", "United Republic of Tanzania", "Zambia", "Zimbabwe")
 
-### years used to project TB in 002_TB_projections
+### years: list of years used to project TB in 002_TB_projections
 # each of the 40 countries has a corresponding year
 years <- c(2011, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2000, 2010, 2000, 2012, 2015, 2000, 2000, 2005, 2000, 2000, 2010, 2000, 2007, 2013, 2015, 2012, 2009, 2011, 2000, 2012, 2000, 2016, 2011, 2009, 2009, 2016, 2000, 2012, 2006, 2000, 2000, 2000)
 names(years) <- c("Angola", "Bangladesh", "Botswana", "Brazil", "Cambodia", "Cameroon", "Central African Republic", "Chad", "China", "Congo", "Democratic People's Republic of Korea", "Democratic Republic of the Congo", "Eswatini", "Ethiopia", "Ghana", "Guinea-Bissau", "India", "Indonesia", "Kenya", "Laos", "Lesotho", "Liberia", "Malawi", "Mozambique", "Myanmar", "Namibia", "Nigeria", "Pakistan", "Papua New Guinea", "Philippines", "Republic of Korea", "Russian Federation", "Sierra Leone", "South Africa", "Thailand", "Uganda", "United Republic of Tanzania", "Vietnam", "Zambia", "Zimbabwe")
 
 ### countries grouped by shape
 # used in 001_TB_incidence_trends
-# 14 linear decrease (Figure CWLD from 001)
+# 14 linear decrease (Figure 1 from 001)
 linear_decrease <- c("Botswana", "Cambodia", "Cameroon", "Chad", "China", "Ethiopia", "Ghana", "India", "Indonesia", "Laos", "Thailand", "Vietnam", "Zambia", "Zimbabwe")
-# 4 increasing (Figure CWII from 001)
+# 4 increasing (Figure S1 from 001)
 increasing <- c("Angola", "Guinea-Bissau", "Liberia", "Mozambique")
-# 5 flat (Figure CWFI from 001)
+# 5 flat (Figure S2 from 001)
 flat <- c("Bangladesh", "Central African Republic", "Nigeria", "Democratic People's Republic of Korea", "Papua New Guinea")
-# 11 peak in the 2000s (Figure CWPITN from 001)
+# 12 peak in the 2000s (Figure 2 from 001)
 peak_in_00s <- c('Angola', 'Cameroon', 'Congo', 'Eswatini', 'Kenya', 'Lesotho', 'Malawi', 'Namibia', 'Sierra Leone', 'South Africa', 'United Republic of Tanzania', 'Zimbabwe')
-# 8 "other" countries (Figures TIFOC and TIFOCNCI from 001)
+# 8 "other" countries (Figures S4 and S5 from 001)
 other <- c("Brazil", "Democratic Republic of the Congo", "Myanmar", "Pakistan", "Philippines", "Republic of Korea", "Russian Federation", "Uganda")
 
 ### countries grouped by whether or not they are projected to meet targets
-
 # will meet
 projected_to_meet_target <- c("Botswana", "Eswatini", "Ethiopia", "Kenya", "Laos", "Lesotho", "Myanmar", "Namibia", "Republic of Korea", "Russian Federation", "South Africa", "United Republic of Tanzania", "Zambia", "Zimbabwe")
 
 # wont meet
-
 projected_to_miss_target <- c("Angola", "Bangladesh", "Brazil", "Cambodia", "Cameroon", "Central African Republic", "Chad", "China", "Congo", "Democratic People's Republic of Korea", "Democratic Republic of the Congo", "Ghana", "Guinea-Bissau", "India", "Indonesia", "Liberia", "Malawi", "Mozambique", "Nigeria", "Pakistan", "Papua New Guinea", "Philippines", "Sierra Leone", "Thailand", "Uganda", "Vietnam")
 
 ### throw warning if input country is not in the 40 countries of interest 
